0f1252381fee0b5d3294c921da6ca1de6c30e56e
Ryan Hellyer <ryanhellyer@gmail.com>
2014-07-25 20:19:46 +0200

    Implementing improved uninstaller, including removing a redundant option removal from an earlier build

--- uninstall.php
+++ uninstall.php
@@ -1,4 +1,18 @@
 <?php
 
-remove_option( 'spam_destroyer_salt' );
+/*
+ * Uninstaller script
+ * Only runs when the plugin is being uninstalled via the WordPress admin panel
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'What you doin?' );
+}
+
+// Remove options
+remove_option( 'spam-killer-stats' );
+remove_option( 'spam-killer-level' );
+
+// Remove network wide options
+remove_site_option( 'spam-killer-comments-blacklist' );
+remove_site_option( 'spam-killer-url-blacklist' );
