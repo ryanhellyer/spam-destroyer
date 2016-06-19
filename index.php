ed65fee2a57464168bcc9e02da1a7791d1688c54
Ryan Hellyer <ryanhellyer@gmail.com>
2016-06-19 13:36:53 +0200

    Adding WordPress loaded check

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 /*
 Plugin Name: Spam Destroyer
-Plugin URI: http://geek.ryanhellyer.net/products/spam-destroyer/
+Plugin URI: https://geek.hellyer.kiwi/products/spam-destroyer/
 Description: Kills spam dead in it's tracks
 Author: Ryan Hellyer
-Version: 2.0.6
-Author URI: http://geek.ryanhelly6er.net/
+Version: 2.0.7
+Author URI: https://geek.hellyer.kiwi/
 
-Copyright (c) 2015 Ryan Hellyer
+Copyright (c) 2012 - 2016 Ryan Hellyer
 
 
 Based on the following open source projects:
@@ -36,7 +36,17 @@ license.txt file included with this plugin for more information.
 
 */
 
+/**
+ * Do not continue processing since file was called directly
+ * 
+ * @since 2.0.7
+ * @author Ryan Hellyer <ryanhellyer@gmail.com>
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Eh! What you doin in here?' );
+}
 
+// Defining folder locations
 define( 'SPAM_DESTROYER_DIR', dirname( __FILE__ ) );
 define( 'SPAM_DESTROYER_URL', plugin_dir_url( __FILE__ ) );
 
@@ -56,7 +66,8 @@ if ( isset( $_GET['captcha'] ) ) {
 if ( is_admin() ) {
 
 	require( 'inc/class-spam-destroyer-settings.php' );
-/*
+
+	// Loading dotorg plugin review code
 	require( 'inc/class-dotorg-plugin-review.php' );
 	new DotOrg_Plugin_Review(
 		array(
@@ -65,5 +76,5 @@ if ( is_admin() ) {
 			'time_limit'  => WEEK_IN_SECONDS,  // The time limit at which notice is shown
 		)
 	);
-*/
+
 }
